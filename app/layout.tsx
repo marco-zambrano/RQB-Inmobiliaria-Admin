f05955ab5673816f669749ed175a5c22e2168f16
marco <marcoadrianzambra@gmail.com>
2026-03-03 23:55:25 -0500

    feat: .ico image

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,6 +50,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="icon" type="image/x-icon" href="https://xpznugqofelwvteosjny.supabase.co/storage/v1/object/sign/rqb-bucket/rqb-icon.ico?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV8xNGFlM2ZmNy1jMDJkLTRmNzUtYWVhYS0wNTY3NzI3YTAxYzYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJycWItYnVja2V0L3JxYi1pY29uLmljbyIsImlhdCI6MTc3MjU5OTc0MCwiZXhwIjoxODM1NjcxNzQwfQ.XLwyq-QgJB7ZwTp1smO6D2D5MYnaSp7K-3HIa6uHw5g" />
+      </head>
       <body className={`${inter.variable} ${playfairDisplay.variable} font-sans antialiased`}>
         {children}
         <Analytics />
